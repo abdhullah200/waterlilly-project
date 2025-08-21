975499968c1cac346d2d21de597ee89708c3b75e
Abdhullah Ariff <139129030+abdhullah200@users.noreply.github.com>
2025-08-22 01:05:18 +0530

    Delete Waterlily.MVC directory

--- Waterlily.MVC/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace Waterlily.MVC.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
